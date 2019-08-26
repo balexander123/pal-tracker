9009dcec3296b2de54419c3dec5c77cb74ba52ac
Barry Alexander <balexander@pivotal.io>
2019-08-25 21:12:28 -0700

    Configuring an App

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class WelcomeController {
 
+    private String greeting;
+
+        public WelcomeController(@Value("${welcome.message}") final String message) {
+        greeting = message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return greeting;
     }
 }
